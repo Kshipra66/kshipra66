52a9faa72f37b8903d9bd3a8aa36fee040180ffd
Kshipra66 <kshipradesai@outlook.com>
2017-03-24 10:04:32 +0530

    Add files via upload

---
+++ WP_A3_66/js/script.js
@@ -0,0 +1,54 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+ name = document.getElementById("mytext").value;
+ if(name == "A")
+{
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+             
+              var attendance = (res.Attend_th + res.Attend_lab)*100/200;
+              var message = 
+               name + "  " + "has"
+              if (attendance<80) {
+                message += "Defaulter";
+              }
+              else {
+                message += " Attendance " +attendance;
+              }
+              
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+}
+else
+{
+  $ajaxUtils
+          .sendGetRequest("data/name2.json", 
+            function (res) {
+             
+              var attendance = (res.Attend_th + res.Attend_lab)*100/200;
+              var message = 
+               name + "  " + "has"
+              if (attendance<80) {
+                message += "Defaulter";
+              }
+              else {
+                message += " Attendance " +attendance;
+              }
+              
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+}
+            });
+      });
+  }
+);
